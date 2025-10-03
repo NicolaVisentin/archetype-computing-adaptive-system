81190527b01d2f7b2de8fe8d95b721b75e8b6589
NicolaVisentin <nicvise2001@gmail.com>
2025-10-03 13:36:28 +0200

    Added comments in script containing function for building torch-compatible dataloaders from MNIST dataset

--- acds/benchmarks/mnist.py
+++ acds/benchmarks/mnist.py
@@ -19,18 +19,24 @@ def get_mnist_data(
             validation. Defaults to 10.
         permuted (bool): Whether to apply permutation to images.
     """
+    # Function to permutate the order of the pixels
     permutation = torch.randperm(28 * 28) if permuted else None 
     def permute_image(image): 
         if permutation is not None:
             image = image.view(-1)[permutation].view(1, 28, 28) 
         return image 
     
+    # Transformations to apply to the images:
+    #   1. Convert images into torch tensor. Shape (C, W, H) C=channels, W=width, H=heigh
+    #   2. Apply custom operation (permutation defined above)
     transform = transforms.Compose([ 
         transforms.ToTensor(), 
         transforms.Lambda(permute_image) 
     ])
 
 
+    # Download (if not already there) full MNIST dataset (70 000 images) in specified folder and build torch
+    # dataset for training (60 000 images) and torch dataset for testing (10 000 images). Also applies tranformations defined above
     train_dataset = torchvision.datasets.MNIST(
         root=root, train=True, transform=transform, download=True
     )
@@ -39,12 +45,14 @@ def get_mnist_data(
         root=root, train=False, transform=transform
     )
 
+    # Split train dataset into actual train dataset and validation dataset according to valid_perc
     valid_size = int(len(train_dataset) * (valid_perc / 100.0))
     train_size = len(train_dataset) - valid_size
     train_dataset, valid_dataset = torch.utils.data.random_split(
         train_dataset, [train_size, valid_size]
     )
 
+    # Build torch dataloaders
     train_loader = DataLoader(dataset=train_dataset, batch_size=bs_train, shuffle=True)
     valid_loader = DataLoader(dataset=valid_dataset, batch_size=bs_test, shuffle=False)
     test_loader = DataLoader(dataset=test_dataset, batch_size=bs_test, shuffle=False)
