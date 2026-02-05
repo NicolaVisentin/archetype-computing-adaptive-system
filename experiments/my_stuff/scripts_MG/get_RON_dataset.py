ff8e230df0d2316a68995e10aebb946c4838f829
NicolaVisentin <nicvise2001@gmail.com>
2026-02-05 21:18:15 +0100

    minor fixes

--- experiments/my_stuff/scripts_MG/get_RON_dataset.py
+++ experiments/my_stuff/scripts_MG/get_RON_dataset.py
@@ -70,11 +70,13 @@ def forw_dynamics(u, y, yd, gamma, epsilon, W, V, b):
 # =========================================================
 
 n_hid = 6 # dimension of the hidden state (number of oscillators) (default RON: 1000)
-architecture_to_test = 'MG_RON_full_6hidden_DT0.05_inpscal0.1' # path of the folder containing trained scaler, model and classifier
+architecture_to_test = 'MG_RON_full_6hidden_DT0.05_INPSCAL1.0' # path of the folder containing trained scaler, model and classifier
 dt = 0.05 # dt of the RON reservoir (default RON: 0.17)
+rho = 0.9 # spectral radius (default RON: 0.9)
+inp_scaling = 1.0 # input matrix scaling (default RON: 10.0)
 m = int(1e5) # dataset dimension (number of datapoints and labels)
-y_range = [-0.2, 0.5] # range of positions to sample. To have an idea about the ranges, take a look at test_RON_model.py
-yd_range = [-0.2, 0.4] # range of velocities to sample. To have an idea about the ranges, take a look at test_RON_model.py
+y_range = [-0.18, 0.08] # range of positions to sample. To have an idea about the ranges, take a look at test_RON_model.py
+yd_range = [-0.2, 0.15] # range of velocities to sample. To have an idea about the ranges, take a look at test_RON_model.py
 # !!! remember to choose the best model when loading the model, scaler and classifier !!!
 
 # ------------
@@ -101,7 +103,7 @@ y = y_min + (y_max - y_min) * torch.rand((m, n_hid), device=device) # m samples 
 yd = yd_min + (yd_max - yd_min) * torch.rand((m, n_hid), device=device) # m samples yd = [yd1, ..., ydN]^T. Shape (m, n_hidden)
 
 # Extract saved model parameters
-model_params = torch.load(model_dir/architecture_to_test/f"{architecture_to_test}_model_4.pt", map_location=device)
+model_params = torch.load(model_dir/architecture_to_test/f"{architecture_to_test}_model_9.pt", map_location=device)
 gamma = model_params["gamma"]
 epsilon= model_params["epsilon"]
 W = model_params["h2h"]
@@ -137,8 +139,8 @@ model = RandomizedOscillatorsNetwork(
     gamma=gamma,
     epsilon=epsilon,
     diffusive_gamma=0.0,
-    rho=0.9,
-    input_scaling=10.0,
+    rho=rho,
+    input_scaling=inp_scaling,
     topology='full',
     reservoir_scaler=1.0,
     sparsity=0.0,
