e65f941efc323cca640d190f209384bd26ed1e39
NicolaVisentin <nicvise2001@gmail.com>
2026-06-22 00:23:25 +0200

    minor change

--- experiments/my_stuff/scripts_sMNIST/get_RON_dataset.py
+++ experiments/my_stuff/scripts_sMNIST/get_RON_dataset.py
@@ -27,7 +27,6 @@ device = (torch.device("cuda")
 # Get relevant paths
 curr_dir = Path(__file__).parent                                                     # current folder
 model_dir = Path(curr_dir.parent/'trained_architectures')                            # folder with the trained architectures to test
-imgs_dir = Path('src/acds/benchmarks/raw')                                           # folder with datasets
 plots_dir = curr_dir.parent/'plots'/curr_dir.stem/Path(__file__).stem                # folder to save plots
 save_results_dir = Path(curr_dir.parent/'results'/curr_dir.stem/Path(__file__).stem) # folder to save data
 
