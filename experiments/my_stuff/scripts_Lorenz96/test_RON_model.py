3f63d1a1749363616160377671168dc24cd0a873
NicolaVisentin <nicvise2001@gmail.com>
2026-06-22 01:43:22 +0200

    Fixed wrong shape of saved input vector.

--- experiments/my_stuff/scripts_Lorenz96/test_RON_model.py
+++ experiments/my_stuff/scripts_Lorenz96/test_RON_model.py
@@ -229,5 +229,5 @@ np.savez(
     y = states_histories[:-2], 
     yd = velocities_histories[:-1], 
     ydd = accelerations_histories[:],
-    u = input_history[:-2, None]
+    u = input_history[:-2]
 )
